0cc530830985483e06c61f11521d2087dd9e7396
lukedamato20 <82449473+lukedamato20@users.noreply.github.com>
2021-05-22 19:09:57 +0200

    updated with send mail code

--- includes/php/send_mail.php
+++ includes/php/send_mail.php
@@ -2,11 +2,23 @@
     session_start();
 
     $email = $_POST['email'];
+    $subject = 'My Favourite List - Our Restaurant';
     $message = $_POST['message'];
+    $list = $_SESSION['list'];
     $food_ID = $_POST['foodID'];
 
+    $message .= "<br>";
+    $message .= file_get_contents("favs.html");
+
+
     # recipient, subject, message, header
-    mail($email, 'My Favourite List - Our Restaurant', $message, 'From: lldamato14@gmail.com');
+    if(mail($email, $subject, $message, 'From: lldamato14@gmail.com')){
+        $_SESSION['sent'] = "email sent";
+    }
+    else{
+        $_SESSION['sent'] = "error sending email";
+    }
+        
 
     // redirecting to details page
     header("location: ../../favs.php?sentemail=success");
