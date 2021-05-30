c817336fce63523bd5440bcc860b1852e1d61329
lukedamato20 <82449473+lukedamato20@users.noreply.github.com>
2021-05-30 16:32:03 +0200

    updated with meta_tags insertion

--- includes/sql/tables.sql
+++ includes/sql/tables.sql
@@ -7,15 +7,15 @@ CREATE TABLE `category` (
 
 -- creating the food table where all the food items and info will be stored
 CREATE TABLE `food` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `category` int(11) NOT NULL,
-    `image` varchar(100) NOT NULL,
-    `name` varchar(50) NOT NULL,
-    `description` varchar(500) NOT NULL,
-    `ingredients` varchar(500) NOT NULL,
-    `price` float(10,2) NOT NULL,
+    'id' int(11) NOT NULL AUTO_INCREMENT,
+    'category' int(11) NOT NULL,
+    'image' varchar(100) NOT NULL,
+    'name' varchar(50) NOT NULL,
+    'description' varchar(500) NOT NULL,
+    'ingredients' varchar(500) NOT NULL,
+    'price' float(10,2) NOT NULL,
+    'meta_tags' varchar(100) NOT NULL,
     PRIMARY KEY (`id`),
     KEY `food_category` (`category`),
     CONSTRAINT `food_category` FOREIGN KEY (`category`) REFERENCES `category` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1
-
